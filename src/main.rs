167187bd468b2c2c4390ced48f0f438c3e53ce6f
SourceBolt <dlockwood@sourceallies.com>
2021-10-01 09:33:06 -0500

    Added initial command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+
+// git diff --shortstat HEAD^ HEAD
+
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let args = vec!["diff", "--shortstat", "HEAD^", "HEAD"];
+    let git_cmd = Command::new("git")
+        .args(args)
+        .output()
+        .expect("Could not get diff");
+
+    println!("Output: {}", String::from_utf8(git_cmd.stdout).expect("Couldn't Parse Utf8"));
 }
