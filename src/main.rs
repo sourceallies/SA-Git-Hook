55ee17ef1993fd6be880939a7a13ea572b90654f
SourceBolt <dlockwood@sourceallies.com>
2021-10-01 10:32:15 -0500

    Added request library and converted to json

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,34 @@ use std::process::Command;
 use std::error::Error;
 use std::str::{FromStr, Split};
 use std::num::ParseIntError;
+use std::fmt::{Display, Formatter};
+
+struct DiffStats {
+    files_changed: u32,
+    insertions: u32,
+    deletions: u32,
+}
+
+impl DiffStats {
+    fn from_string(s: String) -> Result<DiffStats, Box<dyn Error>> {
+        let mut diff_output = s.trim().split(',');
+
+        let files_changed = get_number_from_diff(&mut diff_output)?;
+        let insertions = get_number_from_diff(&mut diff_output)?;
+        let deletions = get_number_from_diff(&mut diff_output)?;
+
+        Ok(DiffStats { files_changed, insertions, deletions })
+    }
+    fn to_json(&self) -> String {
+        format!("{{ \"files_changed\": {}, \"insertions\": {}, \"deletions\": {} }}", self.files_changed, self.insertions, self.deletions)
+    }
+}
+
+impl Display for DiffStats {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "insertions: {}, deletions: {}, files_changed: {}", self.insertions, self.deletions, self.files_changed)
+    }
+}
 
 fn get_number_from_diff(split: &mut Split<char>) -> Result<u32, ParseIntError> {
     u32::from_str(split.next()
@@ -12,17 +40,11 @@ fn get_number_from_diff(split: &mut Split<char>) -> Result<u32, ParseIntError> {
         .unwrap())
 }
 
-fn run_git_cmd(cmd: &mut Command) -> Result<(), Box<dyn Error>> {
+fn run_git_cmd(cmd: &mut Command) -> Result<DiffStats, Box<dyn Error>> {
     let output = cmd.output()?;
     let output_str = String::from_utf8(output.stdout)?;
-    let mut diff_output = output_str.trim().split(',');
 
-    let files_changed = get_number_from_diff(&mut diff_output)?;
-    let insertions = get_number_from_diff(&mut diff_output)?;
-    let deletions = get_number_from_diff(&mut diff_output)?;
-    println!("files changed: {}, insertions: {}, deletions: {}", files_changed, insertions, deletions);
-
-    Ok(())
+    DiffStats::from_string(output_str)
 }
 
 fn main() {
@@ -34,6 +56,8 @@ fn main() {
         Err(e) => {
             println!("Error: {}", e);
         }
-        _ => {}
+        Ok(stats) => {
+            println!("stats: {}", stats.to_json());
+        }
     }
 }
