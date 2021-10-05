1086b5df12b822ff43709686cd7ed9e46b736c6f
SourceBolt <dlockwood@sourceallies.com>
2021-10-05 09:46:22 -0500

    Updated to use uninstall script

--- Hookstaller/src/installer.rs
+++ Hookstaller/src/installer.rs
@@ -61,6 +61,14 @@ fn install_git_hook() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn uninstall_git_hook() -> Result<(), Box<dyn Error>> {
+    let mut git_cmd = Command::new("git");
+    let args = vec!["config", "--global", "--unset", "core.hooksPath"];
+    git_cmd.args(args);
+    git_cmd.output()?;
+    Ok(())
+}
+
 fn log_format<S: AsRef<str>>(output: S) -> String {
     format!("{}: {}", APP_HEADER, output.as_ref())
 }
@@ -70,13 +78,46 @@ fn println_log<S: AsRef<str>>(output: S) {
 }
 
 fn main() {
-    let cfg = match Config::read_input(APP_HEADER) {
+    let cfg = match Config::read_config() {
+        Ok(cfg) => {
+            let should_uninstall_or_override = get_input(log_format("Git Hook Config already exists would you like to uninstall or override (u|o) (Enter to do neither)?"));
+            let should_uninstall =  should_uninstall_or_override.is_ok() && should_uninstall_or_override.as_ref().unwrap().trim().to_lowercase() == "u";
+            if should_uninstall {
+                let deletion = cfg.delete_config();
+                let uninstall = uninstall_git_hook();
+                //TODO: Maybe make these macro rules?
+                match deletion {
+                    Err(e) => {
+                        println_log(e.to_string());
+                    }
+                    _ => {
+                        println_log("Uninstalled Git Hook Config")
+                    }
+                }
+                match uninstall {
+                    Err(e) => {
+                        println_log(e.to_string());
+                    }
+                    _ => {
+                        println_log("Uninstalled Global Git Hook")
+                    }
+                }
+                return;
+            }
+            let should_override = should_uninstall_or_override.is_err() || should_uninstall_or_override.as_ref().unwrap().trim().to_lowercase() == "o";
+            match should_override {true => Config::read_input(APP_HEADER), false => Ok(cfg)}
+        },
+        Err(_) => Config::read_input(APP_HEADER)
+    };
+
+    let cfg = match cfg {
         Ok(cfg) => cfg,
         Err(e) => {
             println_log(e.to_string());
             return;
         }
     };
+
     match cfg.save_to_file() {
         Err(e) => {
             println_log(e.to_string());
